e81616af3869ffa8878e4ead37e181aba1bd0808
sunzid02 <sunzid02@gmail.com>
2022-01-17 14:46:59 +0100

    team generation logic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: Number | '' = 0;
+  teams: string[][] = [];
 
   onInput(member: string){
     this.newMemberName = member;
@@ -17,10 +20,58 @@ export class AppComponent {
 
   }
 
-  addMember(){
-    this.members.push(this.newMemberName)
-    console.log(this.members);
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value);
+    console.log(this.numberOfTeams);
+
   }
 
+  addMember(){
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+
+    this.members.push(this.newMemberName)
+    console.log(this.members);
+    this.newMemberName = '';
+    this.errorMessage = '';
+  }
+
+
+  generateTeams(){
+    if (!this.numberOfTeams || this.numberOfTeams <= 0 || !this.members) {
+      return
+    }
+
+    const allMembers = [...this.members]; //destructure array, ... means rest of the operator
+
+    while (allMembers.length)
+    {
+      for (let i = 0; i < this.numberOfTeams; i++)
+      {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) {
+          break;
+        }
+
+        if (this.teams[i])
+        {
+          this.teams[i].push(member);
+        }
+        else
+        {
+          this.teams[i] = [member];
+        }
+
+      }
+    }
+
+    this.members  = [];
+    this.numberOfTeams = '';
+  }
 
 }
