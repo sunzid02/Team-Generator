ad65ff35d7ad884ce101d6d7a6667dba18bdf13e
sunzid02 <sunzid02@gmail.com>
2022-01-17 11:27:09 +0100

    add members

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'team-app';
+  newMemberName = '';
+  members: string[] = [];
+
+  onInput(member: string){
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+
+  }
+
+  addMember(){
+    this.members.push(this.newMemberName)
+    console.log(this.members);
+  }
+
+
 }
